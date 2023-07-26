6caa4d5f93311d233362adc8f3e6bfe1dd1505eb
Pornchitar Sarsai <kream21574@gmail.com>
2023-07-26 13:07:12 +0700

    first commit

---
+++ main/java/com/pornchitar/labox4/LabOX4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pornchitar.labox4;
+
+/**
+ *
+ * @author ASUS
+ */
+public class LabOX4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
